8da054d75c6eb970d4a957acd4263858c884ddc9
WJASK90 <jaskot.w92@gmail.com>
2024-11-10 15:03:25 +0100

    Update names.py

--- names.py
+++ names.py
@@ -1,2 +1,14 @@
 print("Mam na imię...")  # tutaj wpisz swoje imię
 # i dodaj jeszcze jakiś ciekawy kod
+
+print("Mam na imię Wojtek)
+      
+value_1 = int(input("Podaj początek zakresu liczb, sprawdzimy czy są podzielne przez 5 lub 8: "))
+value_2 = int(input("Podaj koniec zakresu liczb, sprawdzimy czy są podzielne przez 5 lub 8: "))
+for i in range(value_1,value_2):
+    if i % 5 == 0:
+        print(i)
+    elif i % 8 == 0:
+        print(i)
+    else:
+        print(end="")
